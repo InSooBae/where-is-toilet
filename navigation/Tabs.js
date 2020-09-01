138022449901a2ca9842660f9065331ea60c8346
InSooBae <qospwmf@gmail.com>
2020-09-02 01:51:17 +0900

    add navigation title when you click that name [navigation/Tabs.js]

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Search from "../screens/Search";
@@ -6,10 +6,17 @@ import Favs from "../screens/Favs";
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (
-  <Tabs.Navigator>
-    <Tabs.Screen name="Home" component={Home} />
-    <Tabs.Screen name="Search" component={Search} />
-    <Tabs.Screen name="Favs" component={Favs} />
-  </Tabs.Navigator>
-);
+export default ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route?.state?.routeNames[route.state.index] || "Home",
+    });
+  }, [route]);
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name="Home" component={Home} />
+      <Tabs.Screen name="Search" component={Search} />
+      <Tabs.Screen name="Favs" component={Favs} />
+    </Tabs.Navigator>
+  );
+};

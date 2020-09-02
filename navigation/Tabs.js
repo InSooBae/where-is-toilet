d1634aa677da9a7bdccf1e482c72b0330754bd7d
InSooBae <qospwmf@gmail.com>
2020-09-02 19:51:37 +0900

    add icon in Tab and add Setting screen [navigation/Tabs.js | screens/Setting.js]

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,15 +1,19 @@
 import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useColorScheme } from "react-native-appearance";
+import { Ionicons } from "@expo/vector-icons";
 import Home from "../screens/Home";
 import Search from "../screens/Search";
 import Favs from "../screens/Favs";
-
+import { Platform } from "react-native";
+import Setting from "../screens/Setting";
 const Tabs = createBottomTabNavigator();
 
 const getHeaderName = (route) =>
   route?.state?.routeNames[route.state.index] || "Home";
 
 export default ({ navigation, route }) => {
+  const colorScheme = useColorScheme();
   useLayoutEffect(() => {
     const name = getHeaderName(route);
     navigation.setOptions({
@@ -18,10 +22,58 @@ export default ({ navigation, route }) => {
     });
   }, [route]);
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+          if (route.name === "Home") {
+            iconName += "compass";
+          } else if (route.name === "Search") {
+            iconName += "search";
+          } else if (route.name === "Favs") {
+            iconName += "heart";
+          } else if (route.name === "Setting") {
+            iconName += "settings";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              size={24}
+              color={
+                colorScheme === "dark"
+                  ? focused
+                    ? "white"
+                    : "grey"
+                  : focused
+                  ? "black"
+                  : "gray"
+              }
+            />
+          );
+        },
+      })}
+      tabBarOptions={
+        colorScheme === "dark"
+          ? {
+              style: {
+                backgroundColor: "black",
+                borderTopColor: "black",
+              },
+              showLabel: false,
+            }
+          : {
+              style: {
+                backgroundColor: "white",
+                borderTopColor: "white",
+              },
+              showLabel: false,
+            }
+      }
+    >
       <Tabs.Screen name="Home" component={Home} />
       <Tabs.Screen name="Search" component={Search} />
       <Tabs.Screen name="Favs" component={Favs} />
+      <Tabs.Screen name="Setting" component={Setting} />
     </Tabs.Navigator>
   );
 };
